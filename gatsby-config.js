c777a9093daad0083d33f51684a6c7819d74e9d6
Corey Lantz <corey.lantz12@gmail.com>
2019-07-15 20:33:36 -0400

    updating theme colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#00d2a1`,
+        theme_color: `#00d2a1`,
         display: `minimal-ui`,
         icon: `src/images/briana-icon.png`, // This path is relative to the root of the site.
       },
